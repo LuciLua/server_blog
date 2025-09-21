e6af96f7e2dd04be7a4bed78d7912b1a5e8bbe43
Luci Lua <luci.lua81@gmail.com>
2025-09-21 16:59:25 -0300

    secure api

--- server.js
+++ server.js
@@ -11,62 +11,62 @@ const PORT = 3000;
 const MONGO_URI = `mongodb+srv://lucilua81_db_user:${encodeURIComponent(process.env.PASSWORD)}@clusterblog.przjngv.mongodb.net/thelucilens?retryWrites=true&w=majority&appName=clusterblog`;
 
 mongoose.connect(MONGO_URI)
-  .then(() => console.log('MongoDB conectado!'))
-  .catch(err => console.error(err));
+    .then(() => console.log('MongoDB conectado!'))
+    .catch(err => console.error(err));
 
-app.use(cors({allowedHeaders: "*"}));
+app.use(cors({ allowedHeaders: ["https://frontend-blogadmin.vercel.app"] }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
 // Rotas API
 app.get('/api/posts', async (req, res) => {
-  try {
-    const posts = await Post.find().sort({ createdAt: -1 });
-    res.json(posts);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+    try {
+        const posts = await Post.find().sort({ createdAt: -1 });
+        res.json(posts);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 });
 
 app.get('/api/posts/:slug', async (req, res) => {
-  try {
-    const post = await Post.findOne({ slug: req.params.slug });
-    if (!post) return res.status(404).json({ error: 'Post não encontrado' });
-    res.json(post);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+    try {
+        const post = await Post.findOne({ slug: req.params.slug });
+        if (!post) return res.status(404).json({ error: 'Post não encontrado' });
+        res.json(post);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 });
 
 app.post('/api/posts', async (req, res) => {
     console.log("teste1")
-  try {
-    const newPost = new Post(req.body);
-    await newPost.save();
-    res.status(201).json(newPost);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
+    try {
+        const newPost = new Post(req.body);
+        await newPost.save();
+        res.status(201).json(newPost);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
 });
 
 // DELETE /api/posts/:id
 app.delete('/api/posts/:id', async (req, res) => {
-  try {
-    const deletedPost = await Post.findByIdAndDelete(req.params.id);
-    if (!deletedPost) {
-      return res.status(404).json({ error: 'Post não encontrado' });
+    try {
+        const deletedPost = await Post.findByIdAndDelete(req.params.id);
+        if (!deletedPost) {
+            return res.status(404).json({ error: 'Post não encontrado' });
+        }
+        res.json({ message: 'Post deletado com sucesso!' });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
     }
-    res.json({ message: 'Post deletado com sucesso!' });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
 });
 
 app.get('/config', (req, res) => {
-  res.json({ API_URL: process.env.API_URL });
+    res.json({ API_URL: process.env.API_URL });
 });
 
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
