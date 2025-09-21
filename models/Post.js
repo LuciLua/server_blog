4f5f0e9ac3d5d3b1fd5d3057c664a4b45981f07f
Luci Lua <luci.lua81@gmail.com>
2025-09-21 16:12:32 -0300

    server v1

---
+++ models/Post.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  slug: { type: String, required: true, unique: true },
+  excerpt: { type: String, required: true },
+  author: { type: String, required: true },
+  capa: { type: String },
+  content: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Post', PostSchema);
